4ed83c5920b24259102380f7a7cb7b247788de20
Mhnd <mrmohandtv@gmail.com>
2020-10-04 21:09:31 +0300

    edit Venue model

--- starter_code/app.py
+++ starter_code/app.py
@@ -40,8 +40,11 @@ class Venue(db.Model):
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(500))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
